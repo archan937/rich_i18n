b3d92fa9628930f416a66794f8650a8296790802
Paul Engel <paulengel@archan937.local>
2010-10-19 23:55:17 +0200

    Added some manual source file requirements

--- lib/rich_i18n.rb
+++ lib/rich_i18n.rb
@@ -1,5 +1,10 @@
+require "jzip"
+
+require "formtastic"
+ActionView::Base.send :include, Formtastic::SemanticFormHelper
 
 require "rich/i18n"
 require "rich/i18n/core"
 require "rich/i18n/actionpack"
 require "rich/i18n/engine"
+require "rich/i18n/formtastic"
