210f58c72c6158e8dfe45572a9067f5945fe5e34
Paul Engel <paulengel@archan937.local>
2010-11-04 02:28:16 +0100

    Defining String.html_safe when undefined

---
+++ lib/rich/i18n/core/string/html_safe.rb
@@ -0,0 +1,16 @@
+
+module Rich
+  module I18n
+    module Core
+      module String
+        module HtmlSafe
+
+          def html_safe
+            self
+          end
+
+        end
+      end
+    end
+  end
+end
