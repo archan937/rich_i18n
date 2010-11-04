0deb45195d3a0cd7a4b417039dad228c2450fd7d
Paul Engel <paulengel@archan937.local>
2010-11-04 02:04:32 +0100

    Added explicit lib requirements

--- lib/rich_i18n.rb
+++ lib/rich_i18n.rb
@@ -1,10 +1,14 @@
-require "jzip"
-
 require "formtastic"
-ActionView::Base.send :include, Formtastic::SemanticFormHelper
+require "jzip"
 
 require "rich/i18n"
 require "rich/i18n/core"
+require "rich/i18n/activesupport"
 require "rich/i18n/actionpack"
+require "rich/i18n/rails"
 require "rich/i18n/engine"
 require "rich/i18n/formtastic"
+
+if Rails::VERSION::MAJOR < 3
+  ActionView::Base.send :include, Formtastic::SemanticFormHelper unless ActionView::Base.included_modules.include? Formtastic::SemanticFormHelper # still required?
+end
