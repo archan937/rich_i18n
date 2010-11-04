a7fb45b205dfd752ff06a337113fbbd0d699994b
Paul Engel <paulengel@archan937.local>
2010-11-05 00:22:48 +0100

    Cleaned the tests a bit

--- test/tests/core/string/inflections_test.rb
+++ test/tests/core/string/inflections_test.rb
@@ -1,34 +1,33 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "test_helper.rb"))
 
-module Rich
-  module I18n
-    module Test
-      module Core
-        module String
+module Core
+  module String
 
-          class InflectionsTest < ActiveSupport::TestCase
-            test "upcase_first" do
-              assert_equal ""   , "".upcase_first
-              assert_equal "E9s", "e9s".upcase_first
-              assert_equal "E9S", "E9S".upcase_first
-            end
+    class InflectionsTest < ActiveSupport::TestCase
+      test "upcase_first" do
+        assert_equal ""   , "".upcase_first
+        assert_equal "E9s", "e9s".upcase_first
+        assert_equal "E9S", "E9S".upcase_first
+      end
 
-            test "cp_case" do
-              assert_equal "BTW"                   , "btw".cp_case("VAT")
-              assert_equal "huis"                  , "HUIS".cp_case("house")
-              assert_equal "Welkom thuis"          , "Welkom Thuis".cp_case("Welcome home")
-              assert_equal "Welkom bij CodeHero.es", "welkom bij CodeHero.es".cp_case("Welcome at CodeHero.es")
-            end
+      test "cp_case" do
+        assert_equal "BTW"                   , "btw".cp_case("VAT")
+        assert_equal "huis"                  , "HUIS".cp_case("house")
+        assert_equal "Welkom Thuis"          , "Welkom Thuis".cp_case("Welcome home")
+        assert_equal "Welkom bij CodeHero.es", "welkom bij CodeHero.es".cp_case("Welcome at CodeHero.es")
+      end
 
-            test "upcase_first!" do
-              assert_equal nil  , "".upcase_first!
-              assert_equal "E9s", "e9s".upcase_first!
-              assert_equal nil  , "E9s".upcase_first!
-            end
-          end
-        end
+      test "upcase_first!" do
+        assert_equal nil  , "".upcase_first!
+        assert_equal "E9s", "e9s".upcase_first!
+        assert_equal nil  , "E9s".upcase_first!
+      end
 
+      test "singularize!" do
+        assert_equal "QUESTION", "QUESTIONS".singularize!
+        assert_equal nil       , "Question".singularize!
       end
     end
+
   end
 end
