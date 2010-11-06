fdf77f45186787b5d31e27ca365f8a97add39460
Paul Engel <paulengel@archan937.local>
2010-11-06 10:04:53 +0100

    Cleaned up EnrichedString

--- lib/rich/i18n/core/enriched_string.rb
+++ lib/rich/i18n/core/enriched_string.rb
@@ -11,10 +11,6 @@ module Rich
           @meta_data = meta_data || (string.meta_data.dup unless (string.meta_data.nil? rescue true)) || {}
         end
 
-        def enriched_string?
-          true
-        end
-
         def to_es
           (@meta_data.filled? && Engine.can_enrich_output?) ? to_tag : self
         end
@@ -33,7 +29,7 @@ module Rich
 
           attrs << @meta_data[:html].collect{|k, v|      "#{k}=\"#{::ERB::Util.html_escape v}\""}.join(" ") if @meta_data[:html]
           attrs << data             .collect{|k, v| "data-#{k}=\"#{::ERB::Util.html_escape v}\""}.join(" ")
-          attrs << "data-i18n_translation=\"#{::ERB::Util.html_escape @string}\""
+          attrs << "data-i18n_translation=\"#{::ERB::Util.html_escape to_str}\""
 
           "<#{tag} #{attrs.join(" ")}></#{tag}>".html_safe
         end
@@ -44,52 +40,3 @@ module Rich
 end
 
 EnrichedString = Rich::I18n::Core::EnrichedString
-
-#
-# module Rich
-#   module I18n
-#     module Core
-#       class EnrichedString
-#
-#         delegate :empty?, :blank?, :to_json, :+, :<<, :concat, :to => :@string
-#
-#         attr_reader :string, :meta_data
-#
-#         def initialize(string = "", meta_data = nil)
-#           @string    = string
-#           @meta_data = meta_data || (s.meta_data.dup unless (string.meta_data.nil? rescue true)) || {}
-#         end
-#
-#         def enriched_string?
-#           true
-#         end
-#
-#         def to_es
-#           (@meta_data.filled? && Engine.can_enrich_output?) ? to_tag : @string
-#         end
-#         alias_method :to_s, :to_es
-#
-#       private
-#
-#         def to_tag
-#           tag   = :i18n
-#           attrs = []
-#
-#           keys  = [:key, :value, :locale, :derivative_key]
-#           data  = @meta_data.reject{|k, v| !keys.include?(k.to_sym)}
-#
-#           data[:editable_input_type] = @meta_data[:as] if %w(string text html).include? @meta_data[:as].to_s.downcase
-#
-#           attrs << @meta_data[:html].collect{|k, v|      "#{k}=\"#{::ERB::Util.html_escape v}\""}.join(" ") if @meta_data[:html]
-#           attrs << data             .collect{|k, v| "data-#{k}=\"#{::ERB::Util.html_escape v}\""}.join(" ")
-#           attrs << "data-i18n_translation=\"#{::ERB::Util.html_escape @string}\""
-#
-#           "<#{tag} #{attrs.join(" ")}></#{tag}>"
-#         end
-#
-#       end
-#     end
-#   end
-# end
-#
-# EnrichedString = Rich::I18n::Core::EnrichedString
