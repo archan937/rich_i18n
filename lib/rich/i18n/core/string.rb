ce92a8fd11c48bf531f72963b094db2866ccc7d9
Paul Engel <paulengel@archan937.local>
2010-11-06 10:04:00 +0100

    Replaced string/enrichments with string/merging which preserves merged_strings when concatenating Strings, ActiveSupport::SafeBuffers or EnrichedStrings with each other (yay!)

--- lib/rich/i18n/core/string.rb
+++ lib/rich/i18n/core/string.rb
@@ -1,12 +1,12 @@
 
 require "rich/i18n/core/string/inflections"
 require "rich/i18n/core/string/internationalization"
-require "rich/i18n/core/string/enrichments"
+require "rich/i18n/core/string/merging"
 require "rich/i18n/core/string/html_safe"
 
 class String
   include Rich::I18n::Core::String::Inflections
   include Rich::I18n::Core::String::Internationalization
-  include Rich::I18n::Core::String::Enrichments
+  include Rich::I18n::Core::String::Merging
   include Rich::I18n::Core::String::HtmlSafe unless "".respond_to? :html_safe
 end
