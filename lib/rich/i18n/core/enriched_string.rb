b56c7edab1c39c8bc5e28cc2aa603d51e3017303
Paul Engel <paulengel@archan937.local>
2010-11-06 23:58:16 +0100

    Using single quotes in EnrichedString.to_tag

--- lib/rich/i18n/core/enriched_string.rb
+++ lib/rich/i18n/core/enriched_string.rb
@@ -27,11 +27,17 @@ module Rich
 
           data[:editable_input_type] = @meta_data[:as] if %w(string text html).include? @meta_data[:as].to_s.downcase
 
-          attrs << @meta_data[:html].collect{|k, v|      "#{k}=\"#{::ERB::Util.html_escape v}\""}.join(" ") if @meta_data[:html]
-          attrs << data             .collect{|k, v| "data-#{k}=\"#{::ERB::Util.html_escape v}\""}.join(" ")
-          attrs << "data-i18n_translation=\"#{::ERB::Util.html_escape to_str}\""
+          attrs << @meta_data[:html].collect{|k, v|      "#{k}='#{::ERB::Util.html_escape v}'"}.join(" ") if @meta_data[:html]
+          attrs << data             .collect{|k, v| "data-#{k}='#{::ERB::Util.html_escape v}'"}.join(" ")
+          attrs << "data-i18n_translation='#{::ERB::Util.html_escape to_str}'"
+          attrs << "data-i18n_tag='#{@meta_data[:tag]}'" if @meta_data[:tag]
 
           "<#{tag} #{attrs.join(" ")}></#{tag}>".html_safe
+        end
+
+        def to_tag
+          tag = @meta_data[:tag] || (%w(text html).include?(@meta_data[:as].to_s) ? :div : :span if @meta_data[:as])
+          tag ? "<#{tag}>#{self}</#{tag}>".html_safe : self
         end
 
       end
