f5ed7912fcfcbb0269fb3629ba8e92c305563ea6
Paul Engel <paulengel@archan937.local>
2010-11-06 10:04:37 +0100

    Cleaned up string/inflections_test

--- test/tests/core/string/inflections_test.rb
+++ test/tests/core/string/inflections_test.rb
@@ -5,27 +5,26 @@ module Core
 
     class InflectionsTest < ActiveSupport::TestCase
       test "upcase_first" do
-        assert_equal ""   , "".upcase_first
-        assert_equal "E9s", "e9s".upcase_first
-        assert_equal "E9S", "E9S".upcase_first
+        assert_equal      "",      "".upcase_first
+        assert_equal "Value", "value".upcase_first
+        assert_equal "VALUE", "vALUE".upcase_first
       end
 
       test "cp_case" do
-        assert_equal "BTW"                   , "btw".cp_case("VAT")
-        assert_equal "huis"                  , "HUIS".cp_case("house")
-        assert_equal "Welkom Thuis"          , "Welkom Thuis".cp_case("Welcome home")
+        assert_equal                  "VALUE",                  "value".cp_case("KEY")
+        assert_equal                  "value",                  "VALUE".cp_case("key")
         assert_equal "Welkom bij CodeHero.es", "welkom bij CodeHero.es".cp_case("Welcome at CodeHero.es")
       end
 
       test "upcase_first!" do
-        assert_equal nil  , "".upcase_first!
-        assert_equal "E9s", "e9s".upcase_first!
-        assert_equal nil  , "E9s".upcase_first!
+        assert_equal    nil,     "".upcase_first!
+        assert_equal    nil, "Paul".upcase_first!
+        assert_equal "Paul", "paul".upcase_first!
       end
 
       test "singularize!" do
-        assert_equal "QUESTION", "QUESTIONS".singularize!
-        assert_equal nil       , "Question".singularize!
+        assert_equal   nil,  "Key".singularize!
+        assert_equal "KEY", "KEYS".singularize!
       end
     end
 
