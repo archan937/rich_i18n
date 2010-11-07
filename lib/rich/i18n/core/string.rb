e20eaf18c0b29aad340ab2900ef6c26e191d0b17
Paul Engel <paulengel@archan937.local>
2010-11-07 23:37:09 +0100

    Removed core/string/merging

--- lib/rich/i18n/core/string.rb
+++ lib/rich/i18n/core/string.rb
@@ -1,12 +1,10 @@
 
 require "rich/i18n/core/string/inflections"
 require "rich/i18n/core/string/internationalization"
-require "rich/i18n/core/string/merging"
 require "rich/i18n/core/string/html_safe"
 
 class String
   include Rich::I18n::Core::String::Inflections
   include Rich::I18n::Core::String::Internationalization
-  include Rich::I18n::Core::String::Merging
   include Rich::I18n::Core::String::HtmlSafe unless String.new.respond_to? :html_safe
 end
