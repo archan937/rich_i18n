1a1e48a2be30e7dcb0e8d5f203099abed77c68da
Paul Engel <paulengel@archan937.local>
2010-11-08 01:44:54 +0100

    Not explicitly requiring Rich::I18n in the test helper anymore

--- test/rails-2/test_helper.rb
+++ test/rails-2/test_helper.rb
@@ -2,7 +2,4 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "test_help"
 
-require File.join(File.dirname(__FILE__), "..", "..", "lib", "rich_i18n")
-include Rich::I18n
-
 puts "\nRunning Rails #{Rails::VERSION::MAJOR} ...\n\n"
