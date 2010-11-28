7105302f13de7ca46c26e4d23514cc8835d730eb
Paul Engel <paulengel@archan937.local>
2010-11-28 17:17:20 +0100

    Upgraded the Rails 2 dummy app to v2.3.10

--- test/rails-2/test_helper.rb
+++ test/rails-2/test_helper.rb
@@ -2,4 +2,4 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "test_help"
 
-puts "\nRunning Rails #{Rails::VERSION::MAJOR} ...\n\n"
+puts "\nRunning Rails #{Rails::VERSION::STRING}\n\n"
