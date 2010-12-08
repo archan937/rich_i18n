e1e508bb3b1e9a60ccac4359a5807d91833019e0
Paul Engel <paulengel@archan937.local>
2010-12-08 23:13:06 +0100

    Using capybara-envjs which makes executing javascript within the integration test possible (woot!)

--- test/integration_test_helper.rb
+++ test/integration_test_helper.rb
@@ -1,7 +1,12 @@
+# http://techiferous.com/2010/04/using-capybara-in-rails-3 FTW!
+
 require "capybara/rails"
+require "capybara/envjs"
 
 module ActionController
   class IntegrationTest
     include Capybara
   end
 end
+
+Capybara.current_driver = :envjs
--- test/tests/app/root_page_test.rb
+++ test/tests/app/root_page_test.rb
@@ -4,8 +4,9 @@ module App
   class RootPageTest < ActionController::IntegrationTest
 
     context "The root page" do
-      should "return a 200 status" do
+      should "render as expected" do
         visit "/"
+        page.execute_script("$('#page .content .left').append('Hi, my name is Paul Engel!')")
         save_and_open_page
         assert page.has_content?("Test Application")
       end
