0596853677c5a32feb2212a882b92b17e5cd0db8
Paul Engel <paul.engel@holder.nl>
2011-04-24 12:44:11 +0200

    Not adding separator to merged strings when it equals ""

--- lib/rich/i18n/core/array/merging.rb
+++ lib/rich/i18n/core/array/merging.rb
@@ -18,7 +18,7 @@ module Rich
               each_with_index do |x, i|
                 result = "#{result}#{sep unless i.zero?}#{x.enriched? ? x.to_str : x}"
               end
-              any?(&:enriched?) ? ::Translation.new(nil, nil, dup.zip([sep] * (size - 1)).flatten.compact).tap{|t| t.value = result} : result
+              any?(&:enriched?) ? ::Translation.new(nil, nil, dup.zip([(sep unless sep == "")] * (size - 1)).flatten.compact).tap{|t| t.value = result} : result
             end
           end
 
