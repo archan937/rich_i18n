c112d130d5113f74873cff67dda76ad974671d07
Paul Engel <paul.engel@holder.nl>
2011-05-10 20:47:59 +0200

    Updated readme_test.rb (running successfully)

--- suit/shared/test/unit/readme_test.rb
+++ suit/shared/test/unit/readme_test.rb
@@ -14,16 +14,41 @@ class ReadmeTest < ActiveSupport::TestCase
     end
 
     should "return the expected values" do
-      pending do
       [
+
         ["Man / Vrouw",
          "Male / Female".t],
 
-        [Rich::I18n::EnrichedString,
-         "MORE".t.class],
+        ["huis",
+         :house.t],
 
-        [{"locale" => :nl, "value" => "meer", "derivative_key" => "MORE", "key" => "word.more"},
-         "MORE".t.meta_data],
+        ["save",
+         "save".t],
+
+        ["header",
+         "homepage.header".t],
+
+        ["Welcome to our site",
+         "application.index.Welcome_to_our_site".t],
+
+        ["Meer gebruikers",
+         "More users".t],
+
+        ["Meer",
+         "More".t],
+
+        [Translation,
+         "More".t.class],
+
+        [{:derivative_key   => "More",
+          :derivative_value => "Meer",
+          :pluralized       => false,
+          :store_key        => "nl:word.more",
+          :store_value      => nil,
+          :i18n_locale      => "nl",
+          :i18n_key         => "word.more",
+          :i18n_value       => "meer"},
+         "More".t.meta_data],
 
         ["Meer gebruikers",
          "More users".t],
@@ -31,23 +56,24 @@ class ReadmeTest < ActiveSupport::TestCase
         [["Meer", " ", "gebruikers"],
          "More users".t.merged_strings],
 
-        [{},
-         "More users".t.meta_data],
+        [[Translation, String, Translation],
+         "More users".t.merged_strings.collect(&:class)],
 
-        [{"locale" => :nl, "value" => "meer", "derivative_key" => "More", "key" => "word.more"},
-         "More users".t.merged_strings.first.meta_data],
-
-        [{"locale" => :nl, "value" => "gebruiker", "derivative_key" => "users", "key" => ["word.user", "word.users"]},
+        [{:derivative_key   => "users",
+          :derivative_value => "gebruikers",
+          :pluralized       => true,
+          :store_key        => "nl:word.user",
+          :store_value      => nil,
+          :i18n_locale      => "nl",
+          :i18n_key         => "word.user",
+          :i18n_value       => "gebruiker"},
          "More users".t.merged_strings.last.meta_data],
 
-        ["één vraag",
-         "one".t + " " + "question".t],
+        ["<i18n class=\"i18n\" data-store_key=\"nl:word.user\" data-store_value=\"\" data-derivative_key=\"Users\" data-i18n_tag=\"span\" data-i18n_translation=\"Gebruikers\"></i18n>",
+         "Users".t.to_tag],
 
-        [["één", " ", "vraag"],
-         ("one".t + " " + "question".t).merged_strings],
-
-        [true,
-         Rich::I18n::Engine.enable_enriched_output = true],
+        ["<span data-store_key=\"nl:word.user\" class=\"i18n\" data-store_value=\"\" data-derivative_key=\"Users\">Gebruikers</span>",
+         Rich::I18n::Actionpack::ActionView::Sanitizor.sanitize_html("Users".t.to_tag)],
 
         ["huis",
          "house".t],
@@ -66,10 +92,10 @@ class ReadmeTest < ActiveSupport::TestCase
 
         ["Ja / Nee",
          "Yes / No".t]
+
       ].
       each do |(expectation, result)|
         assert_equal expectation, result
-      end
       end
     end
 
@@ -86,10 +112,10 @@ class ReadmeTest < ActiveSupport::TestCase
       element1 = tag.children[1]
       element2 = tag.children[2]
 
-      assert_equal({"data-store_key" => "nl:word.more", "data-derivative_key" => "More", "data-i18n_translation" => "Meer", "data-i18n_tag"=>"span", "data-value" => "", "class" => "i18n"},
+      assert_equal({"data-store_key" => "nl:word.more", "data-derivative_key" => "More", "data-i18n_translation" => "Meer", "data-i18n_tag"=>"span", "data-store_value" => "", "class" => "i18n"},
                    element0.raw_attributes)
       assert_equal " ", element1.to_s
-      assert_equal({"data-store_key" => "nl:word.user", "data-derivative_key" => "users", "data-i18n_translation" => "gebruikers", "data-i18n_tag"=>"span", "data-value" => "", "class" => "i18n"},
+      assert_equal({"data-store_key" => "nl:word.user", "data-derivative_key" => "users", "data-i18n_translation" => "gebruikers", "data-i18n_tag"=>"span", "data-store_value" => "", "class" => "i18n"},
                    element2.raw_attributes)
     end
   end
